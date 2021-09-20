fc62f8c0c18751f2cc2b02c3581cb2fdaf759709
Diogo <72268280+diogorrio@users.noreply.github.com>
2021-09-20 21:30:48 +0100

    Update GameScene.java

--- main/src/GUI/GameScene.java
+++ main/src/GUI/GameScene.java
@@ -1,24 +1,33 @@
 package GUI;
 
+import Board.Board;
+import Board.Spot;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
 import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.text.TextAlignment;
+
+import java.io.*;
 
 
 public class GameScene extends GUIMain {
 
     private static Scene gameScene;
-    private static Label menuLabel;
+    private static Label menuLabel, histTitleLabel, histLabel, playerLabel;
     private static BorderPane gamePane;
-    private static StackPane menuPane;
-    private static VBox menuBox;
+    private static StackPane menuPane, histPane, dicePane, playerPane;
+    private static GridPane boardPane;
+    private static ScrollPane scrollPane;
+    private static Board board;
+    private static VBox menuBox, histBox, scrollBox;
+    private static HBox diceBox, playerBox;
+    private static Image diceImgs[];
+    private static ImageView diceImgViews[];
     private static Button passButton, rollButton, backButton;
+    private static boolean p1Turn;
 
     public GameScene() {
         // Empty.
@@ -26,10 +35,99 @@ public class GameScene extends GUIMain {
 
     public static void setGameScene() {
 
+        board = new Board();
         gamePane = new BorderPane();
         gameScene = new Scene(gamePane, screenBounds.getWidth(), screenBounds.getHeight());
         gameScene.getStylesheets().clear();
         gameScene.getStylesheets().add("/res/Stylesheet.css");
+
+        setMenuPane();
+        setHistPane();
+        setBoardPane();
+        setDicePane();
+        setPlayerPane();
+        setGameButtonsActions();
+
+        gamePane.setLeft(histPane);
+        gamePane.setRight(menuPane);
+        gamePane.setCenter(boardPane);
+        gamePane.setBottom(dicePane);
+        gamePane.setTop(playerPane);
+    }
+
+    private static void setHistPane() {
+
+        histPane = new StackPane();
+        histBox = new VBox(screenBounds.getHeight()/28);
+        histTitleLabel = new Label("MOVES");
+        histTitleLabel.getStyleClass().add("histTitleLabel");
+
+        /*
+        histLabel = new Label("No moves yet\na\na\na\na\na\ng");
+        histLabel.setMinHeight(screenBounds.getHeight()/4);
+        histLabel.setMaxHeight(screenBounds.getHeight()/4);
+        histLabel.getStyleClass().add("histLabel");
+        histLabel.setTextAlignment(TextAlignment.CENTER);
+        */
+
+        scrollBox = new VBox();
+        scrollBox.getStyleClass().add("scrollBox");
+
+        scrollPane = new ScrollPane();
+        scrollPane.setContent(scrollBox);
+        scrollPane.getStyleClass().add("scrollPane");
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        scrollPane.setMinHeight(screenBounds.getHeight()/4);
+        scrollPane.setMaxHeight(screenBounds.getHeight()/4);
+        scrollPane.setFitToHeight(true);
+
+        // Use this to add each move - remove button afterwards, used just for testing
+        /*
+        Button b = new Button("add");
+        b.setOnAction(ev -> {
+            Label newLabel =  new Label("WHATEVER THE MOVE WAS");
+            scrollBox.getChildren().add(newLabel);
+            newLabel.setStyle(
+                    "-fx-font: 15px SansSerifBold;" +
+                    "-fx-font-weight: bold;" +
+                    "-fx-text-fill: white;");
+            newLabel.setAlignment(Pos.CENTER);
+            newLabel.minWidthProperty().bind(Bindings.createDoubleBinding(() ->
+                    scrollPane.getViewportBounds().getWidth(), scrollPane.viewportBoundsProperty()));
+        });
+        */
+        histBox.setAlignment(Pos.CENTER);
+        histBox.getChildren().addAll(histTitleLabel, scrollPane /*, b*/); // remove button afterwards, used for testing
+
+        histPane.getChildren().add(histBox);
+    }
+
+    private static void setBoardPane() {
+
+        boardPane = new GridPane();
+        boardPane.setAlignment(Pos.CENTER);
+
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col ++) {
+                StackPane square = new StackPane();
+                String c;
+                if ((row + col) % 2 != 0) {
+                    c = "black";
+                } else {
+                    c = "white";
+                }
+                square.setStyle("-fx-background-color: " + c + ";");
+                boardPane.add(square, col, row);
+            }
+        }
+        for (int i = 0; i < 8; i++) {
+            boardPane.getColumnConstraints().add(new ColumnConstraints(screenBounds.getWidth()/17.5));
+            boardPane.getRowConstraints().add(new RowConstraints(screenBounds.getHeight()/10));
+        }
+    }
+
+    private static void setMenuPane() {
 
         menuPane = new StackPane();
         menuBox = new VBox(screenBounds.getHeight()/28);
@@ -48,20 +146,71 @@ public class GameScene extends GUIMain {
         menuBox.setAlignment(Pos.CENTER);
         menuBox.getChildren().addAll(menuLabel, passButton, rollButton, backButton);
 
-        setGameButtonsActions();
-
         menuPane.getChildren().add(menuBox);
-        gamePane.setRight(menuPane);
+    }
+
+    private static void setDicePane() {
+
+        dicePane = new StackPane();
+        diceBox = new HBox(screenBounds.getWidth()/20.5);
+        diceImgViews = new ImageView[3];
+        diceImgs = new Image[3];
+        for(int i = 0; i < diceImgs.length; i++) {
+            diceImgs[i] = new Image(GameScene.class.getResourceAsStream("/res/white_pawn.png"));
+            diceImgViews[i] = new ImageView(diceImgs[i]);
+            diceImgViews[i].setFitHeight(screenBounds.getHeight()/11.5);
+            diceImgViews[i].setFitWidth(screenBounds.getWidth()/20.5);
+        }
+
+        diceBox.setAlignment(Pos.TOP_CENTER);
+        diceBox.getChildren().addAll(diceImgViews);
+
+        dicePane.getChildren().add(diceBox);
+    }
+
+    private static void setPlayerPane() {
+
+        playerPane = new StackPane();
+        playerBox = new HBox();
+        String playerString = "Player1" + " vs " + "Player2";
+        p1Turn = true;
+        playerLabel = new Label(playerString);
+        playerLabel.setStyle(
+                "-fx-font: 42px SansSerifBold;" +
+                "-fx-font-weight: bold;" +
+                "-fx-text-fill: linear-gradient(from 60px 60px to 100px 100px, #32cd32, #ff8000);");
+
+        playerBox.setAlignment(Pos.CENTER);
+        playerBox.getChildren().add(playerLabel);
+
+        playerPane.getChildren().add(playerBox);
     }
 
     private static void setGameButtonsActions() {
 
+        // This works (visually) for PvP pre-set settings (names, String, etc) - need to update when we introduce AI
         passButton.setOnAction(e -> {
-
+            if(p1Turn) {
+                playerLabel.setStyle(
+                        "-fx-font: 42px SansSerifBold;" +
+                        "-fx-font-weight: bold;" +
+                        "-fx-text-fill: linear-gradient(from 65px 65px to 100px 100px, #ff8000, #32cd32);");
+                p1Turn = false;
+            } else {
+                playerLabel.setStyle(
+                        "-fx-font: 42px SansSerifBold;" +
+                        "-fx-font-weight: bold;" +
+                        "-fx-text-fill: linear-gradient(from 60px 60px to 100px 100px, #32cd32, #ff8000);");
+                p1Turn = true;
+            }
         });
 
+        // Just to show it's working, need to implement randomness and individual swapping
         rollButton.setOnAction(e -> {
-
+            for(int i = 0; i < diceImgs.length; i++) {
+                diceImgs[i] = new Image(GameScene.class.getResourceAsStream("/res/white_rook.png"));
+                diceImgViews[i].setImage(diceImgs[i]);
+            }
         });
 
         backButton.setOnAction(e -> {
