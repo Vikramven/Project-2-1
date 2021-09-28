c208a60721c8082226234cbf9b8f1a78be17fd19
Diogo <72268280+diogorrio@users.noreply.github.com>
2021-09-28 18:46:47 +0100

    Update GUIMain.java

--- main/src/GUI/GUIMain.java
+++ main/src/GUI/GUIMain.java
@@ -1,5 +1,6 @@
 package GUI;
 
+import Board.Board;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.image.Image;
@@ -7,17 +8,31 @@ import javafx.scene.input.KeyCombination;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-
 public class GUIMain extends Application {
 
     protected static Rectangle2D screenBounds;
     protected static Stage mainStage;
     protected static IntroScene introSc;
     protected static GameScene gameSc;
+    protected static Board board;
 
+    /**
+     * Constructor 1
+     * @param b Loads the same board from the Game class into the GUI package
+     * @param args Arguments passed directly from Game.main()
+     */
+    public GUIMain(Board b, String[] args) {
+        this.board = b;
+        Application.launch(args);
+    }
 
+    /**
+     * Constructor 2
+     * Empty, so that we can create the objects introSc and gameSc - used to set the scenes -
+     * - without having to use static variables OR having to reload the board.
+     */
     public GUIMain() {
-        // Empty.
+        //Empty
     }
 
     @Override
