ce39701912cf80eb530e26f2a93aa11272cfaf27
Diogo <72268280+diogorrio@users.noreply.github.com>
2021-09-28 18:45:43 +0100

    Update Game.java

--- main/src/Game.java
+++ main/src/Game.java
@@ -1,14 +1,23 @@
 import Board.Board;
+import GUI.GUIMain;
+import javafx.application.Application;
+
+import java.awt.desktop.AppForegroundListener;
 
 public class Game {
 
+    private Board board;
+
     public static void main(String[] args) {
         Game game = new Game();
-        //Application.launch(GUIMain.class, args);
+        GUIMain gui = new GUIMain(game.getBoard(),args);
     }
 
-    public Game(){
-        Board board = new Board();
+    public Game() {
+        board = new Board();
         board.print();
     }
+
+    public Board getBoard() { return this.board; }
+
 }
