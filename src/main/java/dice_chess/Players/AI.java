bd7f9da49bf80db27e83e46a142c7305ee4a576a
Diogo <72268280+diogorrio@users.noreply.github.com>
2021-12-05 22:14:24 +0100

    Delete src/main directory

--- src/main/java/dice_chess/Players/AI.java
+++
@@ -1,9 +0,0 @@
-package dice_chess.Players;
-
-public class AI extends Player{
-
-    public AI(boolean blackSide) {
-        super(blackSide, false);
-    }
-
-}
--- src/main/java/dice_chess/Players/Human.java
+++
@@ -1,8 +0,0 @@
-package dice_chess.Players;
-
-public class Human extends Player{
-
-    public Human(boolean blackSide) {
-        super(blackSide, true);
-    }
-}
